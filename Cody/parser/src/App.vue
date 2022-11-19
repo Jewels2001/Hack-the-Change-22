f54f616b3fc69f21e082dc1c9afd834dba4285cf
hodc3780 <hodc@uleth.ca>
2022-11-19 13:59:56 -0700

    added example

--- Cody/parser/src/App.vue
+++ Cody/parser/src/App.vue
@@ -3,25 +3,13 @@
 </template>
 
 <script>
-const fetch = require('node-fetch');
-const { parse } = require('parse5');
-
-const getDescendantByTag = (node, tag) => {
-  for (let i = 0; i < node.childNodes?.length; i++) {
-    if (node.childNodes[i].tagName === tag) return node.childNodes[i];
-
-    const result = getDescendantByTag(node.childNodes[i], tag);
-    if (result) return result;
-  }
-
-  return null;
-};
-
-fetch('https://google.com', { redirect: 'manual' })
-  .then((response) => response.text())
-  .then((text) => {
-    console.log(getDescendantByTag(parse(text), 'title'));
-  });
+fetch('example.html')
+    .then(function (response) {
+      return response.text();
+    })
+    .then(function (text)  {
+      console.log(text);
+    });
 
 export default {
   name: 'App',
