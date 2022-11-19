c67f422b640608cea5f9798ed4bd0d0abda8a07d
Rozella Ricci Dagta <dagta.io@gmail.com>
2022-11-19 13:19:48 -0700

    checking

--- translatorWeb/src/App.vue
+++ translatorWeb/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
--- translatorWeb/src/components/NavBar.vue
+++ translatorWeb/src/components/NavBar.vue
@@ -8,12 +8,10 @@ defineProps({
 </script>
 
 <template>
-  <div class="greetings">
+  <div class="navbar">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+      This is our translator webapp.
     </h3>
   </div>
 </template>
