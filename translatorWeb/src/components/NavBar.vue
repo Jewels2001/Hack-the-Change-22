97609f9d06d3b850074925918db43cc356c807ca
Rozella Ricci Dagta <dagta.io@gmail.com>
2022-11-19 16:54:12 -0700

    cant get tailwind to WORK

--- translatorWeb/src/components/NavBar.vue
+++ translatorWeb/src/components/NavBar.vue
@@ -8,21 +8,23 @@ defineProps({
 </script>
 
 <template>
-  <div class="navbar">
-        <img alt="Vue logo" class="logo" src="@/assets/manyminions.jpg" width="300" height="200" />
+    <div class="text-navy">
+        <img alt="logo" class="logo" src="@/assets/manyminions.jpg" width="300" height="200" />
 
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      This is our translator webapp.
-    </h3>
-          <nav>
+        <h1 class="green">{{ msg }}</h1>
+
+        <h3>
+          This is our translator webapp.
+        </h3>
+          
+        <nav>
             <RouterLink to="/">Home</RouterLink>
             <RouterLink to="/articles">Articles</RouterLink>
             <RouterLink to="/profile">Profile</RouterLink>
             <RouterLink to="/read">Read</RouterLink>
             <RouterLink to="/translate">Translate</RouterLink>
-          </nav>
-  </div>
+        </nav>
+    </div>
 </template>
 
 <style scoped>
--- translatorWeb/tailwind.config.js
+++ translatorWeb/tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: '#ffffff',
+      navy: '#10569E',
+      babyblue: '#78B1EB',
+      gold: '#EBB400',
+      violet: '#492F9E',
+      lavender: '#B5A4EB',
+    },
     extend: {},
   },
   plugins: [],
