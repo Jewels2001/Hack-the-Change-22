f92c44718f48e49aa3d09be88e010bb49e681d7a
hodc3780 <hodc@uleth.ca>
2022-11-19 13:16:54 -0700

    please work

--- Cody/parser/src/App.vue
+++ Cody/parser/src/App.vue
@@ -1,15 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1> parser</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+const fetch = require('node-fetch');
+const { parse } = require('parse5');
+
+const getDescendantByTag = (node, tag) => {
+  for (let i = 0; i < node.childNodes?.length; i++) {
+    if (node.childNodes[i].tagName === tag) return node.childNodes[i];
+
+    const result = getDescendantByTag(node.childNodes[i], tag);
+    if (result) return result;
+  }
+
+  return null;
+};
+
+fetch('https://google.com', { redirect: 'manual' })
+  .then((response) => response.text())
+  .then((text) => {
+    console.log(getDescendantByTag(parse(text), 'title'));
+  });
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
