3fb1141cf7ad08f25217de6facd69792dc13e7c9
Jewels2001 <juliewq25@gmail.com>
2022-11-19 14:51:43 -0700

    Add: Tailwind CSS styling framework

--- translatorWeb/src/App.vue
+++ translatorWeb/src/App.vue
@@ -9,6 +9,9 @@ import AdBar from './components/AdBar.vue'
     <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
     <NavBar />
   </header>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
 
   <RouterView />
 
