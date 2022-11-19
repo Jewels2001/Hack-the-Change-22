1612eea5f62527dc7d393fe167f5068f5e8d143e
Rozella Ricci Dagta <dagta.io@gmail.com>
2022-11-19 13:29:03 -0700

    check

--- translatorWeb/src/App.vue
+++ translatorWeb/src/App.vue
@@ -6,13 +6,13 @@ import NavBar from './components/NavBar.vue'
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
+    <NavBar />
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/about">bong</RouterLink>
       </nav>
     </div>
   </header>
